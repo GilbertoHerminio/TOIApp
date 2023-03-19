eee18a0263452f819919b6f1641d76179f8ee630
Gilberto Araujo <desenvolvedor.gha@gmail.com>
2023-03-19 12:13:12 -0300

    Criação de nova página ContentPage -Tabela Fipe

--- TOIApp/MainPage.xaml.cs
+++ TOIApp/MainPage.xaml.cs
@@ -10,29 +10,27 @@ public partial class MainPage : ContentPage
 
     private void OnfncCalcular(object sender, EventArgs e)
     {
-         lstrResultado.Text = (Convert.ToInt32(lintNumero1.Text)
-                            + Convert.ToInt32(lintNumero2.Text))
-                            .ToString();
+        lstrResultado.Text = (Convert.ToInt32(lintNumero1.Text)
+                           + Convert.ToInt32(lintNumero2.Text))
+                           .ToString();
     }
 
 
     private async void OnfncFrmSobre(object sender, EventArgs e)
     {
-      await Navigation.PushAsync(new frmSobre());
+        await Navigation.PushAsync(new frmSobre());
     }
 
     private async void OnfncFrmAjuda(object sender, EventArgs e)
     {
-        await DisplayAlert("TOI Digital", "Página em construção!", "OK");       
+        await DisplayAlert("TOI Digital", "Página em construção!", "OK");
     }
 
 
-
-
-
-
-
-
+    private async void OnfncFrmTabelaFipe(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new frmTabelaFipe());
+    }
 
 
 }
---
+++ TOIApp/frmTabelaFipe.xaml.cs
@@ -0,0 +1,9 @@
+namespace TOIApp;
+
+public partial class frmTabelaFipe : ContentPage
+{
+	public frmTabelaFipe()
+	{
+		InitializeComponent();
+	}
+}
